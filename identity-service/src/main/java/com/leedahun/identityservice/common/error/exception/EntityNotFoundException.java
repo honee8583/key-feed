f248ff087e9d3b8ff396bbfdda83f1df479b4330
leedahoon <ekgns0508@gmail.com>
2025-11-16 17:21:35 +0900

    chore: identity-service 프로젝트 세팅

---
+++ identity-service/src/main/java/com/leedahun/identityservice/common/error/exception/EntityNotFoundException.java
@@ -0,0 +1,12 @@
+package com.leedahun.identityservice.common.error.exception;
+
+import com.leedahun.identityservice.common.message.ErrorMessage;
+import org.springframework.http.HttpStatus;
+
+public class EntityNotFoundException extends CustomException {
+
+    public EntityNotFoundException(String entity, Object data) {
+        super(ErrorMessage.ENTITY_NOT_FOUND.getMessage() + entity + ": " + data, HttpStatus.BAD_REQUEST);
+    }
+
+}
