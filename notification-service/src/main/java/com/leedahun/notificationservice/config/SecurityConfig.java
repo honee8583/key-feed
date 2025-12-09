9997847a0b2bfac5999c8753b2bffdf18bc785ad
leedahoon <ekgns0508@gmail.com>
2025-12-09 20:10:07 +0900

    feat: 스프링 시큐리티 추가
    - @AuthenticationPrincipal로 로그인한 사용자의 usedId를 가져오기 위함
    - api에 권한을 부여하기 위함

---
+++ notification-service/src/main/java/com/leedahun/notificationservice/config/SecurityConfig.java
@@ -0,0 +1,52 @@
+package com.leedahun.notificationservice.config;
+
+import com.leedahun.notificationservice.common.filter.CustomAccessDeniedHandler;
+import com.leedahun.notificationservice.common.filter.CustomAuthenticationEntrypoint;
+import com.leedahun.notificationservice.common.filter.GatewayHeaderAuthenticationFilter;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.annotation.web.configurers.HeadersConfigurer.FrameOptionsConfig;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+@Configuration
+@EnableWebSecurity
+@RequiredArgsConstructor
+public class SecurityConfig {
+
+    private final CustomAuthenticationEntrypoint authenticationEntrypoint;
+    private final CustomAccessDeniedHandler accessDeniedHandler;
+    private final GatewayHeaderAuthenticationFilter gatewayHeaderAuthenticationFilter;
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+
+        http.headers(headers -> headers.frameOptions(FrameOptionsConfig::disable))
+                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .csrf(AbstractHttpConfigurer::disable)
+                .formLogin(AbstractHttpConfigurer::disable)
+                .httpBasic(AbstractHttpConfigurer::disable);
+
+        http.authorizeHttpRequests(authorize -> authorize
+                .requestMatchers("/api/notifications/**").authenticated()
+
+                // 마이크로서비스간의 통신
+                .requestMatchers("/internal/**").permitAll()
+        );
+
+        http.addFilterBefore(gatewayHeaderAuthenticationFilter, UsernamePasswordAuthenticationFilter.class);
+
+        http.exceptionHandling(e -> e
+                .authenticationEntryPoint(authenticationEntrypoint)
+                .accessDeniedHandler(accessDeniedHandler)
+        );
+
+        return http.build();
+    }
+
+}
