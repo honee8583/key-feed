1d88f7c85445c6235c7e9ea8c27f1016bf3b73cf
leedahoon <ekgns0508@gmail.com>
2025-12-14 16:54:46 +0900

    refactor: snowflake 환경변수 분리

--- match-service/src/main/java/com/leedahun/matchservice/config/SnowFlakeIdConfig.java
+++ match-service/src/main/java/com/leedahun/matchservice/config/SnowFlakeIdConfig.java
@@ -2,6 +2,7 @@ package com.leedahun.matchservice.config;
 
 import cn.hutool.core.lang.Snowflake;
 import cn.hutool.core.util.IdUtil;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -9,8 +10,11 @@ import org.springframework.context.annotation.Configuration;
 public class SnowFlakeIdConfig {
 
     @Bean
-    public Snowflake snowflake() {
-        return IdUtil.getSnowflake(1, 1);
+    public Snowflake snowflake(
+            @Value("${snowflake.worker-id}") long workerId,
+            @Value("${snowflake.datacenter-id}") long datacenterId
+    ) {
+        return IdUtil.getSnowflake(workerId, datacenterId);
     }
 
 }
