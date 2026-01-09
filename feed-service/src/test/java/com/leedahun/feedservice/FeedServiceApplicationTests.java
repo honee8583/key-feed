3a6ae9d1502d43e9dbc971c4e693d8c2bf8607c4
honee8583 <ekgns0508@gmail.com>
2026-01-09 21:56:07 +0900

    fix: feed-service 배포시 테스트 오류 해결

--- feed-service/src/test/java/com/leedahun/feedservice/FeedServiceApplicationTests.java
+++ feed-service/src/test/java/com/leedahun/feedservice/FeedServiceApplicationTests.java
@@ -1,10 +1,15 @@
 package com.leedahun.feedservice;
 
+import com.leedahun.feedservice.domain.feed.repository.ContentDocumentRepository;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
 
 @SpringBootTest
 class FeedServiceApplicationTests {
+
+    @MockitoBean
+    private ContentDocumentRepository contentDocumentRepository;
 
     @Test
     void contextLoads() {
