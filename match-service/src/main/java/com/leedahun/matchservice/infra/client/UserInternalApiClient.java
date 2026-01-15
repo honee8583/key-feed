86f5c17b01d97b3a8a54cb20bd7759e32fa8b308
honee8583 <ekgns0508@gmail.com>
2026-01-15 16:09:10 +0900

    fix: 크롤링한 소스의 sourceId, 포함된 키워드를 구독한 사용자를 불러오도록 수정

--- match-service/src/main/java/com/leedahun/matchservice/infra/client/UserInternalApiClient.java
+++ match-service/src/main/java/com/leedahun/matchservice/infra/client/UserInternalApiClient.java
@@ -4,9 +4,7 @@ import com.leedahun.matchservice.infra.client.dto.KeywordResponseDto;
 import java.util.List;
 import java.util.Set;
 import org.springframework.cloud.openfeign.FeignClient;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.*;
 
 @FeignClient(name = "identity-service")
 public interface UserInternalApiClient {
@@ -15,6 +13,7 @@ public interface UserInternalApiClient {
     List<KeywordResponseDto> getActiveKeywords(@PathVariable("userId") Long userId);
 
     @PostMapping("/internal/keywords/match-users")
-    List<Long> findUserIdsByKeywords(Set<String> keywords);
+    List<Long> findUserIdsByKeywordsAndSource(@RequestBody Set<String> keywords,
+                                              @RequestParam("sourceId") Long sourceId);
 
 }
